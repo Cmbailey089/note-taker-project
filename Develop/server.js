7366be8b28854a3b375c31a228af17f054172732
Cmbailey089 <christopherbailey089@gmail.com>
2023-04-22 21:47:45 -0400

    added helper folder with id generator in it

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const unid = require('./helper/uuid');
+
 
 
 const PORT = process.env.PORT || 3001;
@@ -20,10 +22,38 @@ app.get('*', (req,res) =>
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html')));
 
+app.get('/api/db/db.json', (req,res) => {
+  res.json('/api/db.db.json') 
+  console.info(`${req.method}`)
+});
 
+// POST request to add a review
+app.post('/api/db/db.json', (req, res) => {
+  // Log that a POST request was received
+  console.info();
 
+  // Destructuring assignment for the items in req.body
+  const { title,text } = req.body;
 
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNotes = {
+      title,
+      text,
+      note_id: unid(),
+  };
+  req.body.push(newNotes);
 
+  fs.writeFile(path.join(__dirname, './db/db.json'),JSON.stringify(JSON.parse(data), null, 3),
+    (writeErr) =>
+      writeErr
+        ? console.error(writeErr)
+        : console.info('Successfully updated reviews!')
+  );
+
+   
+  }});
 
 app.listen(PORT, () =>
  console.log(`listening at http://localhost:${PORT}`));
