bce7535782a9d11aa2397b9743c9fb397008f024
Cmbailey089 <christopherbailey089@gmail.com>
2023-04-22 22:35:01 -0400

    append successfully

--- Develop/server.js
+++ Develop/server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/api/notes', (req,res) => {
-  res.json('/api/notes') 
+  res.json(notes) 
   console.info(`${req.method}`)
 });
 
@@ -49,9 +49,9 @@ app.post('/api/notes', (req, res) => {
 
         return res.json(notes);
     });
+ }});
 
-   
-  }});
+
 
 app.listen(PORT, () =>
  console.log(`listening at http://localhost:${PORT}`));
